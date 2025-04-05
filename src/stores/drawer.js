952811aacd55a8e3ff217c12050e57c7ff150c28
Samson Kinara <kinaraishima@gmail.com>
2025-04-05 12:47:17 +0300

    first commit

---
+++ src/stores/drawer.js
@@ -0,0 +1,12 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useDrawerStore = defineStore('drawer', () => {
+  const isOpen = ref(false)
+
+  function toggleDrawer() {
+    isOpen.value = !isOpen.value
+  }
+
+  return { isOpen, toggleDrawer }
+})
